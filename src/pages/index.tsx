d55e87a8f0fb0baac0f6141758116cc72a1e08b4
davi <manelv8@gmail.com>
2021-09-22 23:34:38 -0300

    add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,45 @@
+import { timeLog } from "console";
+import { useEffect, useState } from "react";
+import { Countdown } from "../components/Countdown";
+import { ContainerCountdown,ContainerContent } from '../styles/homeStyle'
 
+let countdownTimeout: NodeJS.Timeout;
 
 export default function Home() {
+  const [time,setTime] = useState(25*60);
+  const[isActive,setIsActive] = useState(false);
+  
+  useEffect(()=>{
+   if(isActive){
+     startCountDown();
+    }
+  },[time]
+  )
+function startCountDown(){
+  countdownTimeout = setTimeout(
+    () => setTime(time - 1),
+    1000
+  )
+}
+  function handleButtonClick(){
+
+  if(isActive){
+    console.log('is')
+    clearTimeout(countdownTimeout)
+    setIsActive(false);
+    setTime(25*60)
+  }else {
+    setIsActive(true)
+    startCountDown()
+  }
+
+  }
+
   return (
-    <h1>hello</h1>
+    <>
+      <ContainerCountdown>
+        <Countdown time={time} isActive={isActive} handleClick={handleButtonClick}/>
+      </ContainerCountdown>
+    </>
   )
 }
---
+++ src/styles/homeStyle.ts
@@ -0,0 +1,19 @@
+import styled,{css} from 'styled-components';
+
+export const ContainerCountdown =styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  margin: 0 auto;
+  
+  max-width: 450px;
+  height: 95vh;
+`
+
+export const ContainerContent = styled.div`
+  display: flex;
+  
+
+`
+
